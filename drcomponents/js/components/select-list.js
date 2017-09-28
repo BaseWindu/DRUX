483342802d31105d65355c7e7cbb0cfc68ab86d6
Cory Pinkney <corypinkney@CorThe45-MacBook-Pro.local>
2017-09-28 12:46:29 -0400

    Select List component

---
+++ drcomponents/js/components/select-list.js
@@ -0,0 +1,31 @@
+export default class SelectList {
+  constructor (options) {
+    this.options = options
+      console.log(this.options)
+  } 
+
+  init() {
+    // create the dom Element
+    let container  = document.createElement( 'div' )
+
+    let selectList = document.createElement('select')
+    selectList.id = this.options.id
+    container.append(selectList)
+
+    this.options.data.forEach( (item)=>{
+        
+      let option = document.createElement('option')
+      option.value = item.value
+      option.append(item.name)
+      selectList.append(option)
+    
+    })
+
+    // create the object3d for this element
+    let cssObject = new THREE.CSS3DObject(container )
+
+      this.object = cssObject
+
+      return(this.object)
+  }
+}
--- drcomponents/js/entry.js
+++ drcomponents/js/entry.js
@@ -1,64 +1,91 @@
 import Coords from './utils/coords'
 import Card from './components/card'
 import TextField from './components/text-field'
+import SelectList from './components/select-list'
 
 export default class Entry {
   constructor() {
     this.coords = new Coords()
-    this.scene = new THREE.Scene()
+      this.scene = new THREE.Scene()
 
-    this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 1, 10000 )
-    this.camera.position.z = 1000
+      this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth/window.innerHeight, 1, 10000 )
+      this.camera.position.z = 1000
 
-    this.renderer = new THREE.CSS3DRenderer()
-    this.renderer.setSize( window.innerWidth, window.innerHeight )
-    this.renderer.domElement.style.position = 'absolute'
-    this.renderer.domElement.style.top = 0
-    this.renderer.domElement.style.zIndex = 0
+      this.renderer = new THREE.CSS3DRenderer()
+      this.renderer.setSize( window.innerWidth, window.innerHeight )
+      this.renderer.domElement.style.position = 'absolute'
+      this.renderer.domElement.style.top = 0
+      this.renderer.domElement.style.zIndex = 0
 
-    this.container = document.getElementById('container')
-    this.container.appendChild(this.renderer.domElement)
+      this.container = document.getElementById('container')
+      this.container.appendChild(this.renderer.domElement)
 
-    /*
-    let card = new Card({
-      width: '800',
-      height: '800',
-      text: 'It works'
-    })
-    */
+      
+         let card = new Card({
+         width: '800',
+         height: '800',
+         text: 'It works'
+         })
+        this.scene.add(card.init()) 
 
-    let name = new TextField({
-      id: 'name',
-      placeHolder: 'Filter by Name...',
-      label: 'Name',
-      layout: {
-        small: 12,
-        medium: 12,
-        large: 6
-      }
-    })
+      /* let name = new TextField({
+         id: 'name',
+         placeHolder: 'Filter by Name...',
+         label: 'Name',
+         layout: {
+         small: 12,
+         medium: 12,
+         large: 6
+         }
+         })
 
-    name.setPosition(this.coords)
+         name.setPosition(this.coords)
 
-    this.scene.add(name.init())
+         this.scene.add(name.init())
 
-    let streetName = new TextField({
-      id: 'streetName',
-      placeHolder: 'Filter by Street Name...',
-      label: 'Street Name',
-      layout: {
-        small: 12,
-        medium: 12,
-        large: 6
-      }
-    })
+         let streetName = new TextField({
+         id: 'streetName',
+         placeHolder: 'Filter by Street Name...',
+         label: 'Street Name',
+         layout: {
+         small: 12,
+         medium: 12,
+         large: 6
+         }
+         })
 
-    this.scene.add(streetName.init())
+         this.scene.add(streetName.init())
+         */
+      let selectList = new SelectList({
+        id: 'Jurisdiction',
+        data: [
+          {
+            name: 'Charlotte',
+            value: 'Charlotte'
+          },
+          {
+            name: 'Miami',
+            value: 'Miami'
+          },
+          {
+            name: 'Indian Trail',
+            value: 'Indian Trail'
+          },
+        ],
+        layout:{
+
+          small: 12,
+          medium: 12,
+          large: 6
+        }
+
+      })
+    this.scene.add(selectList.init())
 
 
-    //this.scene.add(card.init())
+      //this.scene.add(card.init())
 
-    this.renderer.render(this.scene, this.camera)
+      this.renderer.render(this.scene, this.camera)
   }
 }
 
